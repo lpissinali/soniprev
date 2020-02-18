68d5ce986ab9f08ea09cf79b059a1681ee662f09
ShkredovDmitriy <shkredovdmitriy@gmail.com>
2020-02-18 14:50:53 +0300

    fix commits

--- dev/ds-components/ds-components.js
+++ dev/ds-components/ds-components.js
@@ -1,28 +1,3 @@
-/*
-бибоиотека компонентов
-автор shkredovdmitriy@gmail.com
-2018 - 2020
-*/
+import dsMobileMenu from './ds-mobile-menu/ds-mobile-menu';
 
-// -- мини функции хелперы
-// import { dsTruncateHtml } from './helpers-js/ds-truncate'; // обрезка содержимого
-
-// import dsResizeHtmlFontRem from './helpers-js/ds-resize-html-font-rem'; // перещет html font-size от высоты экрана
-
-// -- компоненты
-// import { dsAccordion, dsAccordionManual } from './ds-accordion/ds-accordion'; // аккордион для faq
-
-import dsInputFile from './ds-input-file/ds-input-file'; // -- стилизованный выбор файла
-
-import dsInputPass from './ds-input-pass/ds-input-pass'; // -- стилизованный ввод пароля
-
-import dsSelect from './ds-select/ds-select'; // -- стилизованный html select
-
-import dsTextarea from './ds-textarea/ds-textarea'; // -- блок подстраивается под высоту текста
-
-import dsModal from './ds-modal/ds-modal'; // -- модальное окно
-
-import dsMobileMenu from './ds-mobile-menu/ds-mobile-menu'; // мобильное меню
-
-// -- экспортируем нужные функции
-export { dsInputFile, dsInputPass, dsSelect, dsTextarea, dsModal, dsMobileMenu };
+export { dsMobileMenu };
--- dev/ds-components/ds-mobile-menu/ds-mobile-menu.js
+++ dev/ds-components/ds-mobile-menu/ds-mobile-menu.js
@@ -1,48 +1,36 @@
-/* eslint-disable no-underscore-dangle */
-/* eslint-disable class-methods-use-this */
-/* eslint-disable func-names */
-
-/* -- DS-MOBILE-MENU МОБИЛЬНОЕ МЕНЮ v.2.0.1 */
-
 export default class dsMobileMenu {
   constructor(incomingConfig) {
-    // конфиг по умолчанию
     this.defaultConfig = {
-      container: '.main-menu', // уникальный класс контейнера компонента
-      menuSandwich: '.mob-menu__sandwich', // сендвич меню
-      openButton: '.main-menu__open-button', // кнопки для раскрытия меню
-      closeButton: '.main-menu__close-button', // кнопки для закрытия меню
-      activeClass: 'active', // класс для активации меню
-      bodyOverflow: 'main-menu__overflow-hidden', // класс для блокировки скролла
-      logging: false, // вывод данных в console.log, true / false
+      container: '.main-menu',
+      menuSandwich: '.mob-menu__sandwich',
+      openButton: '.main-menu__open-button',
+      closeButton: '.main-menu__close-button',
+      activeClass: 'active',
+      bodyOverflow: 'main-menu__overflow-hidden',
+      logging: false,
     };
-    // входящий конфиг заменяет дефолтный
+
     this.config = Object.assign({}, this.defaultConfig, incomingConfig);
   }
 
-  // -- ХЕЛПЕРЫ
-  // -- логгирование
   log(mes) {
     if (this.config.logging) {
       console.log(`ds-mobile-menu: ${mes}`);
     }
   }
 
-  // открываем меню
   open() {
     document.querySelector(this.config.menuSandwich).classList.add(this.config.activeClass);
     document.querySelector(this.config.container).classList.add(this.config.activeClass);
     document.querySelector('body').classList.add(this.config.bodyOverflow);
   }
 
-  // закрываем меню
   close() {
     document.querySelector(this.config.menuSandwich).classList.remove(this.config.activeClass);
     document.querySelector(this.config.container).classList.remove(this.config.activeClass);
     document.querySelector('body').classList.remove(this.config.bodyOverflow);
   }
 
-  // подписка на открытие
   addOpenListener() {
     if (document.querySelector(this.config.openButton)) {
       document.querySelector(this.config.openButton).addEventListener('click', () => {
@@ -51,7 +39,6 @@ export default class dsMobileMenu {
     }
   }
 
-  // подписка на закрытие
   addCloseListener() {
     if (document.querySelector(this.config.closeButton)) {
       document.querySelectorAll(this.config.closeButton).forEach(element => {
@@ -62,7 +49,6 @@ export default class dsMobileMenu {
     }
   }
 
-  // подписка на переключатель
   addTriggerListener() {
     if (document.querySelector(this.config.menuSandwich)) {
       document.querySelector(this.config.menuSandwich).addEventListener('click', () => {
@@ -79,7 +65,6 @@ export default class dsMobileMenu {
     this.addOpenListener();
     this.addCloseListener();
     this.addTriggerListener();
-    // -- ОТЧЕТ О СТАРТЕ МОДУЛЯ
     this.log(`initialized, class: ${this.config.container}`);
   }
 }
